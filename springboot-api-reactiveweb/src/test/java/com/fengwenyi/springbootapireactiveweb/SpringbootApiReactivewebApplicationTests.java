3616e7d498c17deeabb3bbc16fbeb8e11b2dd789
冯文议 <xfsy_2015@163.com>
2019-03-13 22:53:29 +0800

    其他也一并提交了

---
+++ springboot-api-reactiveweb/src/test/java/com/fengwenyi/springbootapireactiveweb/SpringbootApiReactivewebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fengwenyi.springbootapireactiveweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootApiReactivewebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
